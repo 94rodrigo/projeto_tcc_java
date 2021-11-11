083c49465dff892c3e34e8740a4a913933e117f0
unknown <rodrigoc94@hotmail.com>
2021-11-11 13:12:24 -0300

    .

--- src/main/java/com/example/demo/api/CoordenadasApi.java
+++ src/main/java/com/example/demo/api/CoordenadasApi.java
@@ -2,12 +2,14 @@ package com.example.demo.api;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.google.maps.GeoApiContext;
 
 @Component
+@Configuration
 public class CoordenadasApi {
 
 	private static final String API_KEY = "GOOGLE_API_KEY";
