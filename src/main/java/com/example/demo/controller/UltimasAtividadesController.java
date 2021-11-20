739d9f6f136feaed1317b643d9ebec6c4ca67ecb
94rodrigo <rodrigoc94@hotmail.com>
2021-11-20 16:45:14 -0300

    .

--- src/main/java/com/example/demo/controller/UltimasAtividadesController.java
+++ src/main/java/com/example/demo/controller/UltimasAtividadesController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -26,16 +27,36 @@ public class UltimasAtividadesController {
 	@GetMapping
 	public String paginaUltimasAtividades(Principal principal, Model model) {
 		User user = userService.findByEmail(principal.getName());
-		List<Atividade> atividadesQueUsuariosParticipa = user.getAtividadesQueUsuariosParticipa();
-		atividadesQueUsuariosParticipa.sort(Comparator.comparing(Atividade::getDataHorarioAtividade));
-		for (Iterator<Atividade> iterator = atividadesQueUsuariosParticipa.iterator(); iterator.hasNext();) {
-			Atividade atividade = iterator.next();
-			if (!atividade.getEstadoAtividade().equals(EstadoAtividade.CONFIRMADO)) {
-				iterator.remove();
-			}
+//		List<Atividade> atividadesQueUsuariosParticipa = user.getAtividadesQueUsuariosParticipa();
+//		List<Atividade> atividadesQueUsuariosParticipou = user.getAtividadesQueUsuariosParticipa();
+		List<Atividade> atividadesQueUsuariosParticipa = new ArrayList<>();
+		List<Atividade> atividadesQueUsuariosParticipou = new ArrayList<>();
+		
+		for (Atividade atividade : user.getAtividadesQueUsuariosParticipa()) {
+			if (atividade.getEstadoAtividade().equals(EstadoAtividade.CONFIRMADO))
+				atividadesQueUsuariosParticipa.add(atividade);
+			else if (atividade.getEstadoAtividade().equals(EstadoAtividade.JÁ_OCORRIDO))
+				atividadesQueUsuariosParticipou.add(atividade);
 		}
+		
+		atividadesQueUsuariosParticipa.sort(Comparator.comparing(Atividade::getDataHorarioAtividade));
+		atividadesQueUsuariosParticipou.sort(Comparator.comparing(Atividade::getDataHorarioAtividade));
+		
+//		for (Iterator<Atividade> iterator = user.getAtividadesQueUsuariosParticipa().iterator(); iterator.hasNext();) {
+//			Atividade atividade = iterator.next();
+//			if (!atividade.getEstadoAtividade().equals(EstadoAtividade.CONFIRMADO)) {
+//				iterator.remove();
+//			}
+//		}
+//		for (Iterator<Atividade> iterator = atividadesQueUsuariosParticipa.iterator(); iterator.hasNext();) {
+//			Atividade atividade = iterator.next();
+//			if (!atividade.getEstadoAtividade().equals(EstadoAtividade.CONFIRMADO)) {
+//				iterator.remove();
+//			}
+//		}
 		model.addAttribute("user", user);
 		model.addAttribute("atividadesQueUsuariosParticipa", atividadesQueUsuariosParticipa);
+		model.addAttribute("atividadesQueUsuariosParticipou", atividadesQueUsuariosParticipou);
 		model.addAttribute("novoUser", new User());
 		return "atividade/ultimas_atividades";
 	}
